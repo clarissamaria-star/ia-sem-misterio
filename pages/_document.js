9ccbe88d1522ded18abe971617bfa018e16ca564
Clarissa Maria <clarissa.maria@sabiaadm.com.br>
2026-06-10 12:02:00 -0300

    Add Meta Pixel directly to document head for reliable tracking

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,31 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
         {/* End Google Tag Manager */}
 
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2686781752624834');
+fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2686781752624834&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel */}
+
         <meta charSet="utf-8" />
         <meta name="description" content="Quiz de qualificação - IA Sem Mistério para Toda Idade. Descubra qual é seu nível e entre na comunidade certa!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
